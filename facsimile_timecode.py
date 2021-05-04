940a69552dbedbcc2a1f7e9d0412760593ed2c6a
rs028 <rob.sommariva@gmail.com>
2021-05-04 20:08:07 +0100

    Script upgrade

--- facsimile_timecode.py
+++ facsimile_timecode.py
@@ -1,34 +1,30 @@
 # #################################################################### #
 #                                                                      #
-# TIME CONTROL CODE                                                    #
+# FACSIMILE TIMECODE                                                   #
 #                                                                      #
-# This program generates the FACSIMILE code for the time control of a  #
-# a model, calculating the correct input and output times              #
-# If the timesteps are irregular it writes the code for the time       #
-# control in the correct format                                        #
+# Script to generate the FACSIMILE code for the time control of a      #
+# FACSIMILE model and calculate the correct input/output times         #
+#                                                                      #
+# If the timesteps are irregular, they must be listed in an input      #
+# file called: 'timecode.in'                                           #
 #                                                                      #
 # #################################################################### #
 #                                                                      #
-# version 1.2, march 2006                                              #
+# version 1.4, may 2021                                                #
 #                                                                      #
 # author: R.S.                                                         #
 #                                                                      #
 # #################################################################### #
 
-# opening message
-print """
-....................................................................
-: TIMECODE 1.0                                                     :
-: calculates the times of inputs and outputs for a FACSIMILE model :
-: and writes the code in FACSIMILE format                          :
-:..................................................................:
-"""
+import textwrap
+import facsimile_funcs
 
-########################################################################
+# #################################################################### #
+
 # function for regular timesteps
 def regular_time():
 
-    # input data
+    # model time information
     gmt = raw_input('enter timezone [+4]: ')
     t_delay = raw_input('enter minutes of delay [02:30]: ')
     start_time = raw_input('enter start hour [00:00]: ')
@@ -67,7 +63,7 @@ def regular_time():
     else:
         n_iter = int(n_iter)
 
-    # conversion to seconds
+    # convert to seconds
     t_delay = t_delay.split(':')
     t_delay = int(t_delay[0])*60 + int(t_delay[1])
 
@@ -81,21 +77,22 @@ def regular_time():
 
     time_step = time_step*60
 
-    # total time of the run
+    # total time of the model run
     run_time = (stop_time-start_time) - time_step
     total_time = (stop_time-start_time)*(n_iter-1) + run_time
 
     # time constant due to minutes of delay and timezone
     time_lag = t_delay + gmt
 
-    # calculate time of the first input and output and the number
-    # of inputs and outputs and times to rewind the input file
+    # time of the first input and output
     first_output = run_time*(n_iter-1) + time_step*(n_iter-1) + time_lag
     n_output = run_time/time_step
 
+    # number of inputs and outputs
     first_input = start_time + time_step + time_lag
     n_input = (total_time-time_step)/time_step
 
+    # number of times to rewind the input file
     rewind_time = run_time + time_step + time_lag
     n_rewind = n_iter - 2
 
@@ -113,23 +110,20 @@ def regular_time():
     print "* ;\n** ;"
     print "----------"
 
-########################################################################
 # function for irregular timesteps
 def irregular_time():
-    
-    # load module with facsimile functions
-    # and open in list file 'timecode.in' containing time steps
-    import textwrap, facsimile_funcs
+
+    # open file 'timecode.in' containing the irregular time steps
     time_points = facsimile_funcs.openlist("timecode.in")
 
-    # put together time steps and FACSIMILE instructions
+    # add time steps to FACSIMILE code
     time_series_out = " ".join(time_points)
     time_series_in = " ".join(time_points[1:])
-    
+
     time_code_out = "TIME = " + time_series_out + " % CALL OUTP ;"
     time_code_in = "TIME = " + time_series_in + " CALL INP RESTART ;"
 
-    #print FACSIMILE code
+    # print FACSIMILE code
     print "\n----------"
     print "FACSIMILE code:"
     print "----------"
@@ -139,48 +133,23 @@ def irregular_time():
     print "* ;\n** ;"
     print "----------"
 
-########################################################################
-# function for irregular timesteps
-def steadystate_time():
-    
-    # load module with facsimile functions
-    # and open in list file 'timecode.in' containing time steps
-    import textwrap, facsimile_funcs
-    time_points = facsimile_funcs.openlist("timecode.in")
+# #################################################################### #
 
-    # put together time steps and FACSIMILE instructions
-    time_series_out = " ".join(time_points)
-    time_series_in = " ".join(time_points[1:])
+print """
+.......................................................
+: FACSIMILE TIMECODE v1.4                             :
+:                                                     :
+: generate the FACSIMILE code for model time control  :
+:.....................................................:
+"""
 
-    print time_series_out
-    print time_series_in
-    
-##     time_code_out = "TIME = " + time_series_out + " % CALL OUTP ;"
-##     time_code_in = "TIME = " + time_series_in + " CALL INP RESTART ;"
-
-##     #print FACSIMILE code
-##     print "\n----------"
-##     print "FACSIMILE code:"
-##     print "----------"
-##     print "* ;\nWHEN"
-##     print textwrap.fill(time_code_out)
-##     print textwrap.fill(time_code_in)
-##     print "* ;\n** ;"
-##     print "----------"
-
-########################################################################
-# menu
-print "choose operation:\n"
 print "[1]\t regular timesteps"
 print "[2]\t irregular timesteps"
-print "[3]\t steady-state timesteps"
 op = raw_input("? ")
 
 if op == "1":
     regular_time()
 elif op == "2":
     irregular_time()
-elif op == "3":
-    steadystate_time()
 else:
     print "----- input error --- program terminated -----"
